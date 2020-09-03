18ec4de5940e4152d37a2b20e6b5cdde08b391d4
Harish Gowda N <n.harishgowda@yahoo.com>
2020-09-03 14:54:06 +0530

    Updates

---
+++ precache-manifest.f8246332076bf60733e768484b4bc4f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b13fe6e9e9440704f537f08d1ded515",
+    "url": "/harish-gowda-n/memayweb/index.html"
+  },
+  {
+    "revision": "5f0e75fd524b732c8270",
+    "url": "/harish-gowda-n/memayweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1706b8d3151c433e7f2a",
+    "url": "/harish-gowda-n/memayweb/static/js/2.7bc6858d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/harish-gowda-n/memayweb/static/js/2.7bc6858d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0e75fd524b732c8270",
+    "url": "/harish-gowda-n/memayweb/static/js/main.4dbd17f1.chunk.js"
+  },
+  {
+    "revision": "e1cd1b239509102450cd",
+    "url": "/harish-gowda-n/memayweb/static/js/runtime-main.090468bd.js"
+  },
+  {
+    "revision": "58d14cc7068071e70815483539b13ef5",
+    "url": "/harish-gowda-n/memayweb/static/media/bg.58d14cc7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/harish-gowda-n/memayweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
