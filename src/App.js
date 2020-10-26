2f0ab5d2880611cd23dd6c7b16028561c32212fa
Harish Gowda N <n.harishgowda@yahoo.com>
2020-10-26 14:07:38 +0530

    Replaced BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
 import MainTile from './MainTile';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router} from 'react-router-dom';
 import PrivacyPolicy from './PrivacyPolicy';
 
 function App() {
@@ -11,10 +11,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Router>
-        <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/privacy-policy" component={PrivacyPolicy} />
-        </Switch>
       </Router>
     </div>
   );
